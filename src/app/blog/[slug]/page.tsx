273606765c21384601a27d9d7b4b1bb4e2ddb432
raipratik <pratikkumar399@gmail.com>
2025-06-28 16:04:19 +0530

    Revert "build fix"
    This reverts commit f7b70b54e41b0f0b4e427666ace155c70659eb90.

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -61,7 +61,12 @@ export default async function Page({ params }: PageProps) {
 
     const { title, author, description, date } = data as BlogFrontmatter;
 
-
+    console.log("Blog Post Data:", {
+        title,
+        author,
+        description,
+        date,
+    });
 
     return (
         <div className="max-w-6xl mx-auto p-4 relative">
